58b75c69d32623a4e6bd427de530f5e68f9e1d57
PaulBorja800 <e12402@ism.edu.ec>
2026-04-10 08:19:14 -0500

    Actualización repositorio

---
+++ Actividad 1.py
@@ -0,0 +1,26 @@
+#Paúl Borja -- 08-04-2026
+#Ejercicio 1
+#Ejercicio 2
+print("La suma de 3+4 es: ", 3+4)
+print("La resta de 4-3 es: ", 4-3)
+print("La multiplicación de 3x4 es: ", 3*4)
+print("El módulo de 4%3 es: ", 4%3)
+print("La división de 4/3 es: ", 4/3)
+print("La evanseventaja de 4**3 es: ", 4**3)
+print("El operador de división de suelo de 4//3 es: ", 4//3)
+
+#Ejercicio 3
+print("Mi nombre es Paúl")
+print("Mi apellido de familia es Borja")
+print("Mi país es Ecuador")
+print("Estoy disfrutando de 30 días de python")
+
+#Ejercicio 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Tu nombre"))
+print(type("Tu apellido de familia"))
+print(type("Tu país"))
---
+++ Actividad 2_Variables.py
@@ -0,0 +1,11 @@
+#Paúl Borja 08-04-2026
+#Muchas variables en una misma linea
+primer_nombre, primer_apellido, pais, edad, esta_casado = "Paúl", "Borja", "Ecuador", 16, False
+print(primer_nombre, primer_apellido, pais, edad, esta_casado)
+print("Primer nombre: ", primer_nombre)
+print("Primer apellido: ", primer_apellido)
+print("País: ", pais)
+print("Edad: ", edad)
+print("Esta casado?: ", esta_casado)
+
+#Tarea
