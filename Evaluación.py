89b0f58b708e5dff49725b4b231efa56d4321640
PaulBorja800 <e12402@ism.edu.ec>
2026-04-20 09:27:24 -0500

    Inicio evaluación

---
+++ Evaluación.py
@@ -0,0 +1,17 @@
+"""Paúl Borja
+Diploma 1 A
+20-04-2026
+Evaluación"""
+
+# ========== Parte A ==========
+#Respuesta 1:
+#Resuesta 2:
+#...
+
+# ========== PARTE B ==========
+# Código corregido 
+# ... 
+
+# ========== PARTE C ========== 
+# Programa integrador 
+# ... 
