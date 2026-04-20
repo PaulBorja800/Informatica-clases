e4114af136929d8a783a07341f4f83173c9472f2
Paul_Borja <e12402@ism.edu.ec>
2026-04-19 21:49:07 -0500

    Tarea 3 finalizada

--- Actividad 4.py
+++ Actividad 4.py
@@ -4,20 +4,60 @@ Diploma 1 A
 """
 #Actividad 4 (Strings)
 # %%
-#Nivel 1
-variable = "Programación Para Todos"
-print(variable)
-print(len(variable))
+# Nivel 1
+texto = "Programación Para Todos"
+# Mostrar contenido
+print(texto)
+# Cantidad de caracteres
+print(len(texto))
 # %%
-#Nivel 2
-variable = "Programación Para Todos"
-print(variable.upper())
-print(variable.lower())
-print(variable.title())
-print(variable.capitalize())
+# Nivel 2
+# Mayúsculas
+print(texto.upper())
+# Minúsculas
+print(texto.lower())
+# Title
+print(texto.title())
+# Capitalize
+print(texto.capitalize())
 # %%
-#Nivel 3
-variable = "Programación Para Todos"
-print(variable.startswith("Programación"))
-print(variable.endswith("Todos"))
-print(variable.find("Para"))
+# Nivel 3
+# Empieza con "Programación"
+print(texto.startswith("Programación"))
+# Termina con "Todos"
+print(texto.endswith("Todos"))
+# Posición de "Para"
+print(texto.find("Para"))
+# Contiene "Python"
+print("Python" in texto)
+# %%
+# Nivel 4
+# Reemplazar
+nuevo_texto = texto.replace("Programación", "Python")
+print(nuevo_texto)
+# Dividir en palabras
+palabras = texto.split()
+print(palabras)
+# Unir con " - "
+unido = " - ".join(palabras)
+print(unido)
+# %%
+# Nivel 5
+# Primer carácter
+print(texto[0])
+# Último carácter
+print(texto[-1])
+# Carácter en posición 5
+print(texto[5])
+# %%
+# Nivel 6
+# Nombre completo
+nombre = "Juan Perez"   # puedes cambiarlo por tu nombre
+# Mensaje
+print("Hola, mi nombre es {}".format(nombre))
+# opción 2
+print(f"Hola, mi nombre es {nombre}")
+# Acrónimo
+partes = nombre.split()
+iniciales = partes[0][0] + partes[1][0]
+print(iniciales)
