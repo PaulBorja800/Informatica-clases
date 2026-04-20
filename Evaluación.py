366a92f88fbf2441818ab7d130e0dfa4c1ad5abe
PaulBorja800 <e12402@ism.edu.ec>
2026-04-20 09:34:04 -0500

    avance parte A

--- Evaluación.py
+++ Evaluación.py
@@ -5,6 +5,30 @@ Evaluación"""
 
 # ========== Parte A ==========
 #Respuesta 1:
+nombre = "Lucía" 
+edad = 16 
+promedio = 9.75 
+cursos = ["Python", "HTML", "CSS"] 
+print(type(nombre)) 
+print(type(edad)) 
+print(type(promedio)) 
+print(type(cursos)) 
+print(len(nombre)) 
+"""Responde: 
+a) Indica el tipo de dato de cada variable:
+nombre = str
+edad = int
+promedio = float
+cursos = list
+b) Escribe qué mostraría el programa en pantalla:
+<class 'str'>
+<class 'int'>
+<class 'float'>
+<class 'list'>
+5
+c) Explica qué hace len(nombre):
+Escribe el número de caracteres que tiene el valor de la variable
+"""
 #Resuesta 2:
 #...
 
