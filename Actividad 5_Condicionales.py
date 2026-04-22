22242c9eace4bdbf504571d3397dd05047e10c84
PaulBorja800 <e12402@ism.edu.ec>
2026-04-22 12:43:25 -0500

    avance de a

--- Actividad 5_Condicionales.py
+++ Actividad 5_Condicionales.py
@@ -1,10 +1,15 @@
 """PaúlBorja
 Diploma 1 A
 22-04-2026"""
-#Ejemplo
-a = 3
+#Inicio clase
+a = int(input("Ingresa un número: ", ))
 if a > 0:
     print('A is a positive number')# A is a positive number
-else:
+elif a < 0:
     print("A is a negative number")# A is a negative number
-#
+elif a/2 == 0:
+    print("A is an even number")# A is an even number
+elif a/2 != 0:
+    print("A is an odd number")# A is an odd number
+else:
+    print("A is 0")#A is 0
