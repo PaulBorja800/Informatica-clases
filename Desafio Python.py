dbbc1d0d94e69e4aa2a5d73d7e9d0da2244f345b
PaulBorja800 <e12402@ism.edu.ec>
2026-04-29 12:04:31 -0500

    desafio

---
+++ Desafio Python.py
@@ -0,0 +1,12 @@
+""" Paúl Borja
+Diploma 1 A
+29-04-2026"""
+#Desafio Python: "Sistema de acceso al club Tecno"
+#Variables:
+nombre = str(input("Ingres3 su nombre: " ))
+edad = int(input("Ingrese su edad: " ))
+puntaje = int(input("Ingrese su puntaje obtenido: " ))
+asistencia = bool(input("¿Asististe a la competencia?: " ))
+codigoInvitacion = str(input("Ingresa tu código de invitación: " ))
+#Prints:
+print(nombre.upper())
