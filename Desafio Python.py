b2058769ae88c600b3e95d3a0d9fe42d24d39a9f
PaulBorja800 <e12402@ism.edu.ec>
2026-04-29 12:21:42 -0500

    fin desafio

--- Desafio Python.py
+++ Desafio Python.py
@@ -9,24 +9,29 @@ puntaje = int(input("Ingrese su puntaje obtenido: " ))
 asistencia = bool(input("¿Asististe a la competencia?(TRUE/FALSE): " ))
 codigoInvitacion = str(input("Ingresa tu código de invitación: " ))
 #Prints:
-print(nombre.upper())
-print(nombre())
+print("Participante: ", nombre.upper())
+print("Caracteres del nombre: ", len(nombre))
 #Promedio:
 promedio = (puntaje + asistencia)/2
+print("Promedio general: ", promedio)
 #Ingresar al evento:
 if edad >= 14:
     if promedio >= 80:
         if codigoInvitacion == "PYTHON2026":
-            print("Acceso VIP")
+            print("Resultado: Acceso VIP")
         else:
-            print("Acceso general")
+            print("Resultado: Acceso general")
     elif 60 < promedio < 79:
-        print("Acceso con observación")
+        print("Resultado: Acceso con observación")
     elif promedio < 60:
-        print("No se puede ingresar por bajo rendimiento")
+        print("Resultado: No se puede ingresar por bajo rendimiento")
 else:
     if codigoInvitacion == "PYTHON2026":
-        print("Acceso especial con acompañante")
+        print("Resultado: Acceso especial con acompañante")
     else:
-        print("No cumple con la edad mínima")
-
+        print("Resultado: No cumple con la edad mínima")
+#Puntaje adicional:
+if puntaje >= 90 and asistencia >= 90:
+    print("Mensaje adicional: Candidato destacado")
+elif puntaje < 50 or asistencia < 50:
+    print("Mensaje adicional: Requiere refuerzo previo")
