8cd8fe4cbc05f94d084202c912ea8d533555eb6a
PaulBorja800 <e12402@ism.edu.ec>
2026-05-04 09:54:05 -0500

    clave

--- Actividad 6_Ciclos de repeticion.py
+++ Actividad 6_Ciclos de repeticion.py
@@ -2,9 +2,16 @@
 Diploma 1A
 04-05-2026"""
 #Ciclos de repetición
+#Variables de control
 count = 0
 while count < 5:
     print(count)
     count = count + 1
 else:
     print(f"Se dieron {count} vueltas .Ciclo terminado")
+#Ingresar clave
+clave = ""
+while clave != "Python":
+    clave = str(input("Ingrese la clave: " ))
+else:
+    print("Acceso permitido")
