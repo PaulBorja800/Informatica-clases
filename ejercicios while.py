6369b3ea2e013b949831e1d32f177ae1968cdf80
PaulBorja800 <e12402@ism.edu.ec>
2026-05-08 08:18:04 -0500

    ejercico 2

--- ejercicios while.py
+++ ejercicios while.py
@@ -3,6 +3,7 @@ Dipoma 1 A
 08-05-2026"""
 #Ejercicios while
 #Ejercicio 1: Suma acumulativa
+print("------- Ejercicio 1 -------")
 num = int(input("Ingresa un número entero positivo: " ))
 control = 1
 sum = 1
@@ -12,6 +13,19 @@ while num > 0:
         sum = sum + control
     else:
         print("La suma de todos los enteros es: ", sum)
+        break
 else:
     print("Número inválido, ingresar un positivo")
 # Ejercicio 2: Control de presupuesto
+print("------- Ejercicio 2 -------")
+cantidad = 0
+sumaTotal = 0
+while True:
+    compras = int(input("Ingresa el valor de tu compra: $" ))
+    if compras <= 0:
+        break
+    cantidad = cantidad + 1
+    sumaTotal = sumaTotal + compras
+print("La cantidad de compras fue de: ", cantidad)
+print("La suma total de las compras es de: $", sumaTotal)
+print("Registro de compras finalizado")
