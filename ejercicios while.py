fee9ac87bd9bbea2493043ba6fb4e164caa5d7e8
PaulBorja800 <e12402@ism.edu.ec>
2026-05-08 07:55:09 -0500

    intento

--- ejercicios while.py
+++ ejercicios while.py
@@ -2,3 +2,12 @@
 Dipoma 1 A
 08-05-2026"""
 #Ejercicios while
+#Ejercicio 1
+num = int(input("Ingresa un número entero positivo: " ))
+control = 1
+sum = 0
+while num > control:
+    control = control + 1
+    sum = control 
+else:
+    print("La suma de todos los enteros es: ", sum)
