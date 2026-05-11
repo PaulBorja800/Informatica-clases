060cc32325de4ab2416e7c03771caca934227686
PaulBorja800 <e12402@ism.edu.ec>
2026-05-11 10:08:30 -0500

    ejemplo 2

--- Actividad 6.1 for.py
+++ Actividad 6.1 for.py
@@ -5,3 +5,10 @@ Diploma 1A
 numbers = [ 0, 1, 2, 3]
 for iterador in numbers:
     print(iterador)
+#Ejemplo 2
+notas = [ 8, 7, 9, 10, 6]
+suma = 0
+for nota in notas:
+    suma = suma + nota
+promedio = suma / len(notas)
+print(promedio)
