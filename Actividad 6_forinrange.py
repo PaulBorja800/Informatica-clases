d7f9df52ec732b471b1de7ed082448969ec9c372
PaulBorja800 <e12402@ism.edu.ec>
2026-05-20 12:07:42 -0500

    tabla

--- Actividad 6_forinrange.py
+++ Actividad 6_forinrange.py
@@ -3,3 +3,10 @@ Diploma 1A
 20-05-2026"""
 
 #For in range
+# Tabla de multiplicar
+print("------- Ejercicio 1 -------")
+numero = int(input("Ingresa un número: " ))
+num = int(input("¿Hasta qué número quieres ver?: " ))
+for i in range (1, num+1):
+    print(f"{numero}{"x"}{i}{"="}{i*numero}")
+
