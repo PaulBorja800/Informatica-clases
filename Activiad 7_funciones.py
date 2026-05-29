3cc0e554e4e9559fde4991d43923f4fc2b976d3a
PaulBorja800 <e12402@ism.edu.ec>
2026-05-29 08:00:55 -0500

    ejemplo 1

--- Activiad 7_funciones.py
+++ Activiad 7_funciones.py
@@ -3,3 +3,12 @@ Diploma 1 A
 29-05-2026
 """
 #Funciones
+# Ejemplo 1:
+print("------- Ejemplo 1 -------")
+def nombre_completo():
+    nombre = "Paúl"
+    apellido = "Borja"
+    espacio = ""
+    nombre_completo = nombre + espacio + apellido
+    print(nombre_completo)
+nombre_completo() # llama a la variable
