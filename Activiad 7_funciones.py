441f09a7235f2ade85a21cc38e59857bc47a0253
PaulBorja800 <e12402@ism.edu.ec>
2026-05-29 08:12:33 -0500

    parametros

--- Activiad 7_funciones.py
+++ Activiad 7_funciones.py
@@ -3,7 +3,7 @@ Diploma 1 A
 29-05-2026
 """
 #Funciones
-# Ejemplo 1:
+# Ejemplo 1(sin parametros):
 print("------- Ejemplo 1 -------")
 def nombre_completo():
     nombre = "Paúl"
@@ -12,3 +12,9 @@ def nombre_completo():
     nombre_completo = nombre + espacio + apellido
     print(nombre_completo)
 nombre_completo() # llama a la variable
+
+# Ejemplo 2(con parametros):
+print("------- Ejemplo 2 -------")
+def saludar (nombre):
+    print(f"Hola, {nombre}!")
+saludar ("Paúl")
