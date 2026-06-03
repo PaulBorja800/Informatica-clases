c52443973d7d6deae5af93ec515051834b862d4f
PaulBorja800 <e12402@ism.edu.ec>
2026-06-03 13:05:45 -0500

    arreglos, salir

--- Actividad 7_funciones2.py
+++ Actividad 7_funciones2.py
@@ -17,9 +17,6 @@ print(f"{obtenerMensaje(mensaje)}, {GenerarNombreCompleto(nombre, apellido)}")
 #Ejercico 2:
 #Genere una calculadora con las 4 operaciones básicas ( suma, resta, multiplicación, división) mediante un procedimiento con parámetros para cada operación, el usuario debe ingresar los 2 números por teclado y seleccionar la operación
 print("------- Ejercicio 1 -------")
-print("Calculadora simple, ingresa 2 números y luego elige la operaciíon que quieras realizar")
-num1 = int(input("Ingresa el primer número: "))
-num2 = int(input("Ingresa el segundo número: "))
 def sumaNumeros(suma):
     return suma
 def restaNumeros(resta):
@@ -28,19 +25,25 @@ def multiplicacionNumeros(multiplicacion):
     return multiplicacion
 def divisionNumeros(division):
     return division
-operacion = int(input("¿Qué operación quieres hacer?(1.Suma, 2.Resta, 3.Multiplicación, 4.División): "))
-division = num1 / num2
-multiplicacion = num1 * num2
-resta = num1 - num2
-suma = num1 + num2
-if operacion == 1:
-    print(f"La respuesta de la suma es: {sumaNumeros(suma)}")
-elif operacion == 2:
-    print(f"La respuesta de la resta es: {restaNumeros(resta)}")
-elif operacion == 3:
-    print(f"La respuesta de la multiplicación es: {multiplicacionNumeros(multiplicacion)}")
-elif operacion == 4:
-    print(f"La respuesta de la división es: {divisionNumeros(division)}")
-else:
-    print("Opción inválida, ingresar número del 1 al 4")
-print("Gracias por usar")
+print("Calculadora simple, ingresa 2 números y luego elige la operaciíon que quieras realizar")
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+while True:
+    operacion = int(input("¿Qué operación quieres hacer?(1.Suma, 2.Resta, 3.Multiplicación, 4.División, 5.Salir): "))
+    division = num1 / num2
+    multiplicacion = num1 * num2
+    resta = num1 - num2
+    suma = num1 + num2
+    if operacion == 1:
+        print(f"La respuesta de la suma es: {sumaNumeros(suma)}")
+    elif operacion == 2:
+        print(f"La respuesta de la resta es: {restaNumeros(resta)}")
+    elif operacion == 3:
+        print(f"La respuesta de la multiplicación es: {multiplicacionNumeros(multiplicacion)}")
+    elif operacion == 4:
+        print(f"La respuesta de la división es: {divisionNumeros(division)}")
+    elif operacion == 5:
+        print("Gracias por utilizar")
+        break
+    else:
+        print("Opción inválida")
