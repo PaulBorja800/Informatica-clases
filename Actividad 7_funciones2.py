fb4271131331a5e426f37da01a39c5223c786d1e
PaulBorja800 <e12402@ism.edu.ec>
2026-06-03 12:09:51 -0500

    actividad 7 parte 2

---
+++ Actividad 7_funciones2.py
@@ -0,0 +1,17 @@
+"""Paúl Borja
+Diploma 1 A
+03-06-2026
+"""
+#Funciones
+#Ejemplo Básico:
+def obtenerMensaje():
+    mensaje = "Bienvenido al sistema"
+    return mensaje
+def GenerarNombreCompleto():
+    nombre = "Paúl"
+    apellido = "Borja"
+    espacio = ""
+    nombreCompleto = nombre + espacio + apellido
+    return nombreCompleto
+print(obtenerMensaje())
+print(GenerarNombreCompleto())
