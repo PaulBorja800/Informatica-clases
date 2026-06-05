b3f92cc174626f6ff8ae9c346144ebf98a26b4b8
PaulBorja800 <e12402@ism.edu.ec>
2026-06-05 07:48:38 -0500

    Tarea

--- Actividad 7_funciones2.py
+++ Actividad 7_funciones2.py
@@ -47,3 +47,40 @@ while True:
         break
     else:
         print("Opción inválida")
+
+#Tarea:
+def calcular_promedio(n1, n2, n3):
+    return (n1 + n2 + n3) / 3
+def nota_mayor(n1, n2, n3):
+    return max(n1, n2, n3)
+def nota_menor(n1, n2, n3):
+    return min(n1, n2, n3)
+def estado_estudiante(n1, n2, n3):
+    promedio = calcular_promedio(n1, n2, n3)
+    if promedio >= 7:
+        return "Aprueba"
+    else:
+        return "Reprueba"
+nota1 = float(input("Ingrese la primera calificación: "))
+nota2 = float(input("Ingrese la segunda calificación: "))
+nota3 = float(input("Ingrese la tercera calificación: "))
+print("------- MENÚ DE CALIFICACIONES -------")
+print("1. Calcular el promedio")
+print("2. Mostrar la nota mayor")
+print("3. Mostrar la nota menor")
+print("4. Determinar si el estudiante aprueba o reprueba")
+opcion = int(input("Seleccione una opción: "))
+if opcion == 1:
+    promedio = calcular_promedio(nota1, nota2, nota3)
+    print(f"El promedio del estudiante es: {promedio:.2f}")
+elif opcion == 2:
+    mayor = nota_mayor(nota1, nota2, nota3)
+    print(f"La nota más alta es: {mayor}")
+elif opcion == 3:
+    menor = nota_menor(nota1, nota2, nota3)
+    print(f"La nota más baja es: {menor}")
+elif opcion == 4:
+    estado = estado_estudiante(nota1, nota2, nota3)
+    print(f"El estudiante: {estado}")
+else:
+    print(f"La opción {opcion} no es válida.")
